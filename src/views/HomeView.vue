5c0679f9be42681eafb1d982ba4b31b504ba8a47
ryodgie <ryodgie@gmail.com>
2023-03-01 23:43:05 +0800

    refs, reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,68 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h3>{{ counterData.title }}</h3>
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text" />
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref, reactive } from "vue";
+
+// const counter = ref(0),
+//   counterTitle = ref("My Counter :");
+
+const counterData = reactive({
+  count: 0,
+  title: "My Counter",
+});
+
+const increaseCounter = () => counterData.count++;
+const decreaseCounter = () => counterData.count--;
+</script>
+
+<!--
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const counter = ref(0);
+
+    const increaseCounter = () => counter.value++;
+    const decreaseCounter = () => counter.value--;
+
+    return {
+      counter,
+      increaseCounter,
+      decreaseCounter,
+    };
+  },
+};
+</script>
+-->
+
+<style>
+.home {
+  text-align: center;
+  padding: 1.25rem;
+}
+
+.btn,
+.counter {
+  font-size: 2.5rem;
+  margin: 0.625rem;
+}
+
+.edit {
+  margin-top: 3.75rem;
+}
+</style>
